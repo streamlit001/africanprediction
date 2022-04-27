93f540a626e0d961a00af1d75629cd80673e4d63
vermaayush680 <vermaayush680@gmail.com>
2022-04-27 15:53:47 +0530

    Fixed Everything

--- predictions.py
+++ predictions.py
@@ -8,12 +8,34 @@ from sklearn.pipeline import Pipeline
 from keras.preprocessing import image
 from PIL import Image
 import streamlit as st
+st.set_page_config(page_title='African Wildlife', page_icon='favicon.png')
 
 #Function to load uploaded image
 def load_image(image_file):
 	img = Image.open(image_file)
 	return img
 # Function to check the image
+def set_bg_hack_url():
+    '''
+    A function to unpack an image from url and set as bg.
+    Returns
+    -------
+    The background.
+    '''
+        
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url("https://wallpaperaccess.com/full/2757600.jpg");
+             background-size: cover;
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+@st.cache(ttl=48*3600)
 def check():
 
     lr = keras.models.load_model('weights.h5')
@@ -47,28 +69,64 @@ def output(full_pipeline,img):
    predic = full_pipeline.predict(a)
    return(predic)
 
+def home():
+  st.write("## CNN Image Detection Using EfficientNetB6 Model")
+  st.write("#### We passed this image for prediction through our EfficientNetB6 model")
+  st.image("original.jpg", width=800)
+  st.write("#### This is the output, our model predicted")
+  st.image("predict111.jpg", width=800)
+#   st.image("predictions_img2.jpg", width=800)
+  st.write('## To know more about the project, visit the About page')
+
+def about():
+  st.write("# Team SMOTE with Real-Time Animal Detection using CNN")
+  st.write("### All the files for this project can be found on our Github Repository [here](https://github.com/heyakshayhere/Hamoye_capstone_project_smote/)")
+
+
+def predict():
+    st.subheader('Upload either Buffalo/Elephant/Rhino/Zebra image for prediction')
+    image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+    # code for Prediction
+    prediction = ''
+
+    # creating a button for Prediction
+
+    if st.button('Predict'):
+        if image_file is not None:
+            # To See details
+            with st.spinner('Loading Image and Model...'):
+                full_pipeline = check()
+            file_details = {"filename":image_file.name, "filetype":image_file.type,"filesize":image_file.size}
+            st.write(file_details)
+            img = load_image(image_file)
+            st.image(img,width=256)
+            with st.spinner('Predicting...'):
+                prediction = output(full_pipeline,img)
+            st.success(prediction)
+
 def main():
-   # giving a title
-   st.set_page_config(page_title='African Wildlife', page_icon='favicon.png')
-   st.title('African Wildlife Animal Classifier')
-   st.subheader('Upload either Buffalo/Elephant/Rhino/Zebra image for prediction')
-   image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
-   # code for Prediction
-   prediction = ''
-
-   # creating a button for Prediction
-
-   if st.button('Predict'):
-     if image_file is not None:
-         # To See details
-         with st.spinner('Loading Image and Model...'):
-            full_pipeline = check()
-         file_details = {"filename":image_file.name, "filetype":image_file.type,"filesize":image_file.size}
-         st.write(file_details)
-         img = load_image(image_file)
-         st.image(img,width=256)
-         with st.spinner('Predicting...'):
-            prediction = output(full_pipeline,img)
-         st.success(prediction)
+    hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden; }
+        footer {visibility: hidden;}
+        </style>
+        """
+    st.markdown(hide_menu_style, unsafe_allow_html=True)
+    # giving a title
+   
+    st.write('<style>.css-10trblm{ text-align:center;} .css-12oz5g7 { max-width: 50rem;} div.row-widget.stRadio > div{flex-direction:row;justify-content: space-evenly;} div.row-widget.stRadio > div > label > div{font-size:24px !important;}</style>',unsafe_allow_html=True)
+    st.title('African Wildlife Animal Classifier')
+    page = st.radio("", ["Home","Predict","About"])
+    # Website flow logic    
+    set_bg_hack_url()
+    if page == "Home":
+        
+        home()
+    elif page == "Predict":
+        predict()
+    elif page == "About":
+        set_bg_hack_url()
+        about()
+   
 if __name__ == '__main__':
     main()
